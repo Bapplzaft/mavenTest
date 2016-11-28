b4b0930c4b9d4134063b4b5bd9e6d6100e27c777
Florian <florian.hauger@hs-furtwangen.de>
2016-11-28 17:02:15 +0100

    Kommentare

--- src/main/java/Florian/App.java
+++ src/main/java/Florian/App.java
@@ -4,16 +4,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Klasse der main-Funktion.
+ */
 public class App {
+	/**
+	 * Hauptprogramm liest einen String ein und gibt diesen in Großbuchstaben
+	 * aus.
+	 */
 	public static void main(String[] args) {
-        try {
+		try {
 			String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
 			System.out.println(s.toUpperCase());
-        } catch (IOException e) {
+		} catch (IOException e) {
 
-        	e.printStackTrace();
+			e.printStackTrace();
 		}
-	
+
 	}
 }
-
